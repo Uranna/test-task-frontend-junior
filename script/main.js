b37378a4338939f200accad6cd5c7f16019320c2
Anna Urakova <gram_anna@mail.ru>
2021-08-07 21:22:21 +0400

    feat: revision according to the terms of reference

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+// Массивы постов
 let news=[
   {id:"animate", content:"Мульт в кино. Выпуск №103. Некогда грустить!", image:"img/image-0.jpg", desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
   {id:"batman", content:"Новый Бэтмен", image:"img/image-1.jpg", desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
@@ -17,32 +18,69 @@ let header=document.getElementById("header");
 let footer=document.getElementById("footer");
 let nav=document.getElementById("tabs");
 
-let str=`<div class="logo">
-  <img src="img/logo.svg" alt="">
-  <p class="title">Видеосервис</p>
-</div>
-<div class="search">
-  <input type="text" placeholder="Поиск...">
-  <a href="#">Найти</a>
-</div>
-<button class="btn" onClick="login()">Войти</button>`;
+
+// Если есть ник пользователя в localStorage (вход в аккаунт был совершен) подгружаем данные о пользователе и кнопку выхода, иначе кнопку входа.
+let user="";
+
+if (localStorage.hasOwnProperty('user'))
+{
+  user= ` <input type="text" id="name" value="${localStorage.name}" onBlur="editName()">
+          <button class="log-out" onClick="out()">Выйти</button>`;
+}
+else {
+  user= `<button class="btn" onClick="login()">Войти</button>`;
+}
+
+// ЗАГРУЗКА ШАПКИ СТРАНИЦЫ
+let str=  `<div class="logo">
+            <img src="img/logo.svg" alt="">
+            <p class="title">Видеосервис</p>
+          </div>
+          <div class="search">
+            <input type="text" placeholder="Поиск...">
+            <a href="#">Найти</a>
+          </div>
+          <div id="authorization">
+            ${user}
+          </div>`;
 header.insertAdjacentHTML("afterBegin", str);
 
-str=`<section class="size" itemscope="" itemtype="https://schema.org/Organization">
-  <img src="img/htc.svg" alt="" itemprop="logo" class="organization">
-  <p class="organization" itemprop="address">426057, Россия, Удмуртская Республика, г. Ижевск, ул. Карла Маркса, 246 (ДК «Металлург»)</p>
-  <p class="organization" itemprop="telephone"><a href="tel:+73412938861">+7 (3412) 93-88-61</a>, <a href="tel:432929">43-29-29</a></p>
-  <p class="organization" itemprop="url"><a href="https://htc-cs.ru">htc-cs.ru</a></p>
-</section>`;
+// ЗАГРУЗКА ПОДВАЛА СТРАНИЦЫ
+str=` <section class="size" itemscope="" itemtype="https://schema.org/Organization">
+        <img src="img/htc.svg" alt="" itemprop="logo" class="organization">
+        <p class="organization" itemprop="address">426057, Россия, Удмуртская Республика, г. Ижевск, ул. Карла Маркса, 246 (ДК «Металлург»)</p>
+        <p class="organization" itemprop="telephone"><a href="tel:+73412938861">+7 (3412) 93-88-61</a>, <a href="tel:432929">43-29-29</a></p>
+        <p class="organization" itemprop="url"><a href="https://htc-cs.ru">htc-cs.ru</a></p>
+      </section>`;
 footer.insertAdjacentHTML("afterBegin", str);
 
+// ЗАГРУЗКА МОДАЛЬНОГО ОКНА
+str=`<div id="bg">
+      <div class="close"></div>
+      <div class="modal">
+        <form action="" method="post" name="modal">
+          <h1>Вход</h1>
+          <input type="text" placeholder="Логин" id='user' class='authorization-form'>
+          <input type="password" placeholder="Пароль" id='password' class='authorization-form'>
+          <label class="checkbox">
+            <input type="checkbox" id="remember">
+            <span class="checkbox-custom"></span>
+            <span class="checkbox-text">Запомнить</span>
+          </label>
+          <button class="btn" type="submit">Войти</button>
+        </form>
+      </div>
+    </div>`;
+footer.insertAdjacentHTML("afterEnd", str);
+
+// ЗАГРУЗКА НАВИГАЦИИ
 str=`<ul>
-  <li><a href="index.html" id="selectMovie">Фильмы</a></li>
-  <li><a href="tv.html" id="selectTv">Программа</a></li>
-</ul>`;
+      <li><a href="index.html" id="selectMovie">Фильмы</a></li>
+      <li><a href="tv.html" id="selectTv">Программа</a></li>
+    </ul>`;
 nav.insertAdjacentHTML("afterBegin", str);
 
-
+// ВЫДЕЛЕНИЕ АКТИВНОЙ СТРАНИЦЫ В НАВИГАЦИИ
  if(nav.classList.contains("activeMovie")){
    document.querySelector("#selectMovie").classList.add("active");
  }
@@ -50,19 +88,20 @@ nav.insertAdjacentHTML("afterBegin", str);
    document.querySelector("#selectTv").classList.add("active");
  }
 
-
-
+// СОДЕРЖИМОЕ СТРАНИЦЫ "ФИЛЬМЫ"
 if (document.querySelector("#selectMovie").classList.contains("active")){
   let films=document.getElementById("new");
   news.forEach((item, i) => {
     let str=`
     <article class="movie" id="${item.id}">
-      <div class="description">
-        <img src="${item.image}" class="movie-icon" alt="">
-        <img src="img/fade.svg" alt="" class="gray">
-        <p>${item.desc}</p>
-      </div>
-      <h2>${item.content}</h2>
+      <a href="#">
+        <div class="description">
+          <img src="${item.image}" class="movie-icon" alt="">
+          <img src="img/fade.svg" alt="" class="gray">
+          <p>${item.desc}</p>
+        </div>
+        <h2>${item.content}</h2>
+      </a>
     </article>`;
     films.insertAdjacentHTML("beforeEnd", str);
   });
@@ -72,8 +111,10 @@ if (document.querySelector("#selectMovie").classList.contains("active")){
   genres.forEach((item, i) => {
     let str=`
     <article class="genre" id="${item.id}">
-     <div class="icon">${item.icon}</div>
-     <p>${item.name}</p>
+      <a href="#">
+        <div class="icon">${item.icon}</div>
+        <p>${item.name}</p>
+      </a>
     </article>`;
     films.insertAdjacentHTML("beforeEnd", str);
     let elem=document.getElementById(`${item.id}`);
@@ -81,9 +122,37 @@ if (document.querySelector("#selectMovie").classList.contains("active")){
   });
 }
 
+
+// ПОЯВЛЕНИЕ МОДАЛЬНОГО ОКНА
 function login(){
   document.querySelector("#bg").style.display="flex";
 }
+// ЗАКРЫТИЕ МОДАЛЬНОГО ОКНА
 document.querySelector(".close").onclick=function(){
   document.querySelector("#bg").style.display="none";
 }
+
+// КНОПКА "ВЫЙТИ" ИЗ АВТОРИЗОВАННОЙ ШАПКИ
+function out(){
+   localStorage.clear();
+   document.getElementById("authorization").innerHTML = "";
+   document.getElementById("authorization").insertAdjacentHTML('afterBegin','<button class="btn" onClick="login()">Войти</button>')
+}
+
+// ПЕРЕЗАПИСЬ ИМЕНИ ПОЛЬЗОВАТЕЛЯ
+function editName(){
+  let name=document.getElementById("name").value;
+  localStorage.name=name;
+};
+
+
+
+document.forms.modal.onsubmit = function() {
+Array.from(document.querySelectorAll('.authorization-form')).forEach((item, i) => {
+      localStorage[item.id]=item.value;
+    });
+
+    localStorage["name"]=localStorage['user'];
+    // alert('test');
+
+};
