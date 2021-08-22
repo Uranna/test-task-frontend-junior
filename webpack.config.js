5da4cc34dcf82a6caa896984a6b76ebdbdd8c648
Urakova <gram_anna@mail.ru>
2021-08-23 02:24:43 +0400

    Добавлен WebPack. Отредактирован код. Исправлены возникшие ошибки. Обновлен README

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader",
+                    "sass-loader",
+                ],
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js'
+    },
+    devServer: {
+        port: 5000
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, './src/img/'), to: path.resolve(__dirname, 'dist/img') }
+            ],
+        }),
+    ]
+}
